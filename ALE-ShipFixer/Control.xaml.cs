d983b8966c4b0e4f55a05db1f546ada77b5d3fdf
LordTylus <martin.roekker@gmail.com>
2022-04-24 15:26:07 +0200

    even more code style

--- ALE-ShipFixer/Control.xaml.cs
+++ ALE-ShipFixer/Control.xaml.cs
@@ -1,39 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ALE_ShipFixer
-{
+namespace ALE_ShipFixer {
 
-    public partial class Control : UserControl
-    {
+    public partial class Control : UserControl {
 
         private ShipFixerPlugin Plugin { get; }
 
-        public Control()
-        {
+        public Control() {
             InitializeComponent();
         }
 
-        public Control(ShipFixerPlugin plugin) : this()
-        {
+        public Control(ShipFixerPlugin plugin) : this() {
             Plugin = plugin;
             DataContext = plugin.Config;
         }
 
-        private void SaveButton_OnClick(object sender, RoutedEventArgs e)
-        {
+        private void SaveButton_OnClick(object sender, RoutedEventArgs e) {
             Plugin.Save();
         }
     }
